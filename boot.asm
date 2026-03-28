1ef562a1e925bcd7fee659992712cc46f9843050
Mahesh <umamashesh006@gmailcom>
2026-03-28 16:36:04 +0000

    Kernel with C

--- boot.asm
+++ boot.asm
@@ -1,86 +1,55 @@
+[bits 16]
+; No [org] here if using a linker, but since we will compile to bin, use:
 [org 0x7c00]
-KERNEL_OFFSET equ 0x1000 
-;global start
-;extern kmain
 
-start:
-
-    mov [BOOT_DRIVE], dl    ; BIOS automatically stores boot device type (floppy - 0x00, HDD - 0x80) in dl register
-
-    xor ax, ax    ; Set AX to 0
-    mov ds, ax    ; DS = 0
-    mov es, ax    ; ES = 0
-    mov ss, ax    ; SS = 0
-    mov sp, 0x7c00 ; Stack starts below the bootloader
-
-    mov ax, 0xb800    ; AX is just a 16-bit accumulator register. The VGA "window" starts at 0xb8000
-    mov es, ax        ; ES (Extra Segment) now points to Video Memory. We cant directly move an immediate into es register.
-
-    
-
-    
-
-    xor ax, ax    ; Set AX to 0
-    mov ds, ax    ; DS = 0
-    mov es, ax    ; ES = 0
-    mov ss, ax    ; SS = 0    
-    mov si, ax    ; SS = 0    
-
-    ; Setup a simple stack for the BIOS to work
+start_boot:
+    mov [BOOT_DRIVE], dl
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7c00
+    mov ax, 0xb800
+    mov es, ax    
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov si, ax
     mov bp, 0x9000
     mov sp, bp
-
     call load_from_disk
-
-    jmp KERNEL_OFFSET 
-
-
-
-
+    jmp 0x1000 
 
 [bits 16]
 load_from_disk:
-
     push dx
     xor ax, ax          ; Reset ES to 0
     mov es, ax
     mov ah, 0x00        ; Reset disk system
     int 0x13
     pop dx
-
-    mov ah, 0x02            ; BIOS "Read Sectors" function
-    mov al, 16              ; Number of sectors to read (increase this as your big file grows)
-    mov ch, 0x00            ; Cylinder 0
-    mov dh, 0x00            ; Head 0
-    mov cl, 0x02            ; Start reading from sector 2 (Sector 1 is this bootloader)
-    mov dl, [BOOT_DRIVE]    ; The drive we saved earlier
-    mov bx, KERNEL_OFFSET   ; Destination address in RAM (0x1000)
-    int 0x13                ; BIOS Interrupt for Disk I/O
-    jc disk_error           ; If Carry Flag is set, the hardware failed
+    mov ah, 0x02 
+    mov al, 16           
+    mov ch, 0x00         
+    mov dh, 0x00         
+    mov cl, 0x02         
+    mov dl, [BOOT_DRIVE] 
+    mov bx, 0x1000
+    int 0x13             
+    jc disk_error        
     ret
 
 disk_error:
-
     mov bh, ah          ; Save error code in BH
-    
     mov ah, 0x0e
     mov al, 'E'         ; 'E' for Error
     int 0x10
-    
     mov al, bh          ; Move error code to AL
     add al, '0'         ; To convert to ASCII digit. ASCII numbers, start from 0. And we get a number for error.
     int 0x10
-    
     jmp $
 
-
 BOOT_DRIVE db 0
-
-; BOOT_DRIVE db 0 is not a command for the CPU; it is an instruction for the Assembler (NASM).
-; This is a label. It acts as a human-friendly name for a memory address. When you compile, NASM replaces this name with the actual offset (e.g., 0x7c3e).
-; db: Stands for Define Byte. It tells the assembler, "Reserve exactly 1 byte of space right here."
-; 0: This is the initial value placed in that byte.
-
- 
 times 510-($-$$) db 0       ; Pad to 510 bytes
 dw 0xaa55                   ; The Magic Number
--- kernel.c
+++ kernel.c
@@ -1,10 +1,18 @@
-// This is your new "main"
-void kmain() {
-    // Create a pointer to the VGA buffer
-    // Volatile tells the compiler: "Don't optimize this away, the hardware is watching!"
-    volatile char* vga_buffer = (char*) 0xb8000;
 
-    // Write 'C' to the top left
-    vga_buffer[0] = 'C'; 
-    vga_buffer[1] = 0x0F; // White on Black
+void printf(char* message){
+    volatile char* vga_buffer = (char*) 0xb8000;
+    int i = 0;
+    while (message[i] != '\0')
+    {
+        vga_buffer[2*i] = message[i];
+        vga_buffer[2*i + 1] = 0x0F;
+        i++;
+    }
 }
+
+
+void kmain() {
+    printf("Whoaaa!! Its working..... It will work :)");
+    while (1) {}    
+}
+
