e1d6c3f4c8c7c4c190000cca7e3fefa4bd84e44d
Mahesh <umamashesh006@gmailcom>
2026-03-30 01:10:39 +0100

    Calci

--- kernel.c
+++ kernel.c
@@ -1,3 +1,8 @@
+# define padding 9
+# define color 0x0E
+
+volatile char* vga = (char*) 0xb8000;
+
 
 void show(char* message, int line){
     unsigned int location = 0xb8000 + 160*line;
@@ -19,7 +24,7 @@ void end_interrupt();
 
 char scancode_to_ascii[128] = {
     0,    27,  '1', '2', '3', '4', '5', '6',        // 0x00 - 0x07
-    '7',  '8', '9', '0', '-', '=', '\b', '\t',      // 0x08 - 0x0F
+    '7',  '8', '9', '0', '-', '=', '\b', '\t',      // 0x08 - color
     'q',  'w', 'e', 'r', 't', 'y', 'u', 'i',        // 0x10 - 0x17
     'o',  'p', '[', ']', '\n', 0,   'a', 's',       // 0x18 - 0x1F
     'd',  'f', 'g', 'h', 'j', 'k', 'l', ';',        // 0x20 - 0x27
@@ -44,28 +49,148 @@ char scancode_to_ascii[128] = {
     0,    0,   0,   0,   0,   0,   0,   0
 };
 
-volatile int pos = 0;
-
-
-
-void scroll_screen() {
-    volatile char* vga = (char*)0xB8000;
-
-    for (int i = 0; i < 24 * 80 * 2; i++) {
-        vga[i] = vga[i + 160];
+/*
+int ctoi(char number) {
+    int i;
+    for (i = 1; i <= 10; i++)
+    {
+        if (number == scancode_to_ascii[i]) break;
     }
+    return i;
+}
+*/
 
-    for (int i = 24 * 80 * 2; i < 25 * 80 * 2; i += 2) {
-        vga[i] = ' '; 
-        vga[i + 1] = 0x0F;
+
+int ctoi(char c) {
+    if (c >= '0' && c <= '9') {
+        return c - '0';  // '1' (49) - '0' (48) = 1
     }
+    return 0;
 }
 
 
 
+char result_text[5];            // took max result is 9999
+
+
+// below need to be generalised
+void itoc(int number) {
+    result_text[0] = (number / 1000) + '0';
+    result_text[1] = ((number % 1000) / 100) + '0';
+    result_text[2] = ((number % 100) / 10) + '0';
+    result_text[3] = (number % 10) + '0';
+    result_text[4] = '\0';
+}
+
+
+
+volatile int line = 1;
+volatile int offset = padding;
+
+int position;
+
+
+char input_buffer[80]; // Stores up to 80 characters of a command
+volatile int buffer_index = 0;
+
+char output_buffer[80];
+
+
+
+
+void clear_buffer(char buffer[]) {
+    for (int i = 0; i < 80; i++)
+    {
+        buffer[i] = '\0';
+    }
+    
+}
+
+void fill_output(char* result) {
+    int i = 0;
+    while (result[i] != '\0')
+    {
+        output_buffer[i] = result[i];
+        i++;
+    }    
+
+    int j = 0;
+
+    while (result_text[j] != '\0')
+    {
+        output_buffer[i] = result_text[j];
+        j++;
+        i++;
+    }
+    
+}
+
+unsigned int var_a = 0;
+unsigned var_b = 0;
+
+// strictly, Input format: a 56 89
+
+void read_op() {
+    int i = 2;
+    while (input_buffer[i] != ' ')
+    {
+        var_a = var_a * 10 + ctoi(input_buffer[i]);
+        i++;
+    }
+
+    i++;
+    while (input_buffer[i] != ' ')
+    {
+        var_b = var_b * 10 + ctoi(input_buffer[i]);
+        i++;
+    }    
+
+    unsigned int result;    
+   
+    
+    switch (input_buffer[0])
+    {
+    case 'a':
+        result = var_a + var_b;
+
+        itoc(result);
+        fill_output("ADD result: ");
+        break;
+
+    case 's':
+        result = var_a - var_b;
+
+        itoc(result);
+        fill_output("SUB result: ");
+        break;   
+
+    case 'm':
+        result = var_a * var_b;
+
+        itoc(result);
+        fill_output("MUL result: ");
+        break;
+        
+    case 'd':
+        result = var_a / var_b;
+
+        itoc(result);
+        fill_output("DIV result: ");
+        break;        
+    
+    default:
+        fill_output("Invalid operation!!");
+        break;
+    }    
+   
+}
+
+
 void handle_keyboard() {
     unsigned char scancode = port_byte_in(0x60);
-    volatile char* vga = (char*) 0xb8140;
+
+
+    position = line*160 + 2*offset;
 
 
     if (scancode & 0x80) {
@@ -75,39 +200,54 @@ void handle_keyboard() {
 
     switch (scancode) {
         case 0x0E: // Backspace
-            if (pos > 0) {
-                pos = pos - 2;
-                vga[pos] = ' ';
-                vga[pos + 1] = 0x0F;
+            if (offset > padding) {
+                offset--;
+                buffer_index--; // Important: Step back in the buffer too!
+                input_buffer[buffer_index] = '\0'; // Clear the character in the buffer                
+                vga[line*160 + 2*offset] = ' ';
+                vga[line*160 + 2*offset + 1] = color;     
+                
 
-                vga[pos + 2] = ' ';
-                vga[pos + 3] = 0x0F;                    
-            }
+            } 
             break;
 
         case 0x1C: // Enter key 
-            vga[pos] = ' ';
-            vga[pos + 1] = 0x0F;         
-            pos = 160*(((pos) / 160)+1);
+            input_buffer[buffer_index] = ' ';
+
+            line++;
+
+            read_op();
+            show(output_buffer, line);
+            buffer_index = 0;
+            clear_buffer(input_buffer);
+            clear_buffer(output_buffer);
+
+            line++ ;        
+
+            var_a = 0;
+            var_b = 0;            
+
+            show("Command> ", line);
+            offset = padding;
             break;
 
 
         default:
             if (scancode_to_ascii[scancode] != 0) {
-                vga[pos] = scancode_to_ascii[scancode];
-                vga[pos + 1] = 0x0F;
-                pos = pos + 2;
+                vga[position] = scancode_to_ascii[scancode];
+                vga[position + 1] = color;
+                offset++;
+
+
+                input_buffer[buffer_index] = scancode_to_ascii[scancode];
+                buffer_index++;
             }
             break;
     }
 
-    if (pos > 22*160 - 1) {
-        scroll_screen();
-        pos = pos - 160;
-    }
 
-        vga[pos] = '_';
-        vga[pos + 1] = 0x0F;    
+    //vga[position + 1] = '_';
+    //vga[position + 3] = color;
 
     end_interrupt();
 }
@@ -117,13 +257,53 @@ void handle_keyboard() {
 
 
 void kmain() {
-    show("Whoaaa!! Its working..... It will work :)", 0);
-    show("You cant come here. Type something:-", 1);
+    show("Command Line", 0);
 
-    volatile char* vga = (char*) 0xb8140;
+    show("Command> ", 1);
 
     while (1) {
 
     }    
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+void scroll_screen() {
+    volatile char* vga = (char*)0xB8000;
+
+    for (int i = 0; i < 24 * 80 * 2; i++) {
+        vga[i] = vga[i + 160];
+    }
+
+    for (int i = 24 * 80 * 2; i < 25 * 80 * 2; i += 2) {
+        vga[i] = ' '; 
+        vga[i + 1] = color;
+    }
+}
+*/
+
+
+/*
+int pow(int base, int power) {
+    int i = 0;
+    int value = 1;
+    while (i < power)
+    {
+        value = value*base;
+        i++;
+    }
+    return value;
+}*/
